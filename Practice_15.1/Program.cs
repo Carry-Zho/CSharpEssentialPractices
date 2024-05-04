9b63410d3e59c8f904c6f00e02ce14006eca4266
karry <karry@outlook.com>
2024-05-04 18:10:51 +0800

    Add something

--- Practice_15.1/Program.cs
+++ Practice_15.1/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace Practice_15._1
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice_15._1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> saySomething = new List<string>
+            {
+                "Hi",
+                "CSharp",
+                "the best programing language"
+            };
+
+            Print(saySomething);
+            Console.ReadKey();
+        }
+        private static void Print<T>(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
