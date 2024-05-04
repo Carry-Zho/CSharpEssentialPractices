aa3eb7b88609a081f5a5fc5968c456bad6162e30
karry <karry@outlook.com>
2024-05-04 20:44:24 +0800

    Completed

--- Practice_15.2/Program.cs
+++ Practice_15.2/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Practice_15._2
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
+
+namespace Practice_15._2
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, ConsoleColor> colorMap = new Dictionary<string, ConsoleColor>
+            {
+                ["Error"] = ConsoleColor.Red,
+                ["Warning"] = ConsoleColor.Yellow,
+                ["Information"] = ConsoleColor.Green,
+                ["Verbose"] = ConsoleColor.White
+            };
+            foreach(KeyValuePair<string, ConsoleColor> color in colorMap)
+            {
+                Console.WriteLine(color.Key + ":"+ color.Value);
+            }
+            Console.ReadLine();
         }
     }
 }
