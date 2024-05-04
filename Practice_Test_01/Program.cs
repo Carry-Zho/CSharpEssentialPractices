ad102fda71962d9e3bf28372690a706fc1438591
karry <karry@outlook.com>
2024-05-04 23:27:35 +0800

    Add Test_01

---
+++ Practice_Test_01/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice_Test_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>()
+            {
+                ["Name"] = "CSharp",
+                ["Version"] = "1.0",
+                ["Author"] = "Author"
+            };
+            while (Dictionary<string, string>.Enumerator) 
+            {
+            }
+        }
+    }
+}
