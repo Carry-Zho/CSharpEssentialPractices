b0645e9259848efebf6a75201eca78b3e2e658f2
karry <karry@outlook.com>
2024-05-04 23:13:43 +0800

    Completed 15.3

--- Practice_15.3/Program.cs
+++ Practice_15.3/Program.cs
@@ -5,8 +5,19 @@
         static void Main(string[] args)
         {
             int[] array = new int[]{ 1, 2, 3, 4, 5, 6 };
-            foreach (int item in array) 
+
+            //语法糖
+            foreach (int item in array)
             {
+                Console.WriteLine(item);
+            }
+
+            //编译器创建的等价for循环
+            int[] tempArray;
+            tempArray = array;
+            for(int counter = 0; (counter < array.Length); counter++) 
+            {
+                int item = tempArray[counter];
                 Console.WriteLine(item);
             }
         }
