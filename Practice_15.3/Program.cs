a11cca77769852f6b2b2ee7874acfdbad7652a51
karry <karry@outlook.com>
2024-05-04 20:48:06 +0800

    Add 15.3

---
+++ Practice_15.3/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Practice_15._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[]{ 1, 2, 3, 4, 5, 6 };
+            foreach (int item in array) 
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
