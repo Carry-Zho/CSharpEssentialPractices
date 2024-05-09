dc3cc633bb72784cc3b99fdec0282941661d97eb
karry <karry@outlook.com>
2024-05-09 22:59:28 +0800

    Close 15.16

---
+++ Practice_15.16/Patent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_15._16
+{
+    public class Patent
+    {
+        public string Title { get; set;} //Title of the published application
+        public string YearOfPublication { get; set;} //The date the application was officially published
+        public string ApplicationNumber { get; set;} //A unique number assigned to published applications
+        public long[] InventorIds { get; set;}
+
+        public override string ToString()
+        {
+            return $"{Title} ({YearOfPublication})";
+        }
+    }
+}
---
+++ Practice_15.16/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Practice_15._16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Patent> patents = PatentData.Patents;
+            bool result;
+            patents = patents.Where(patent => 
+            {
+                if (result = patent.YearOfPublication.StartsWith("18"))
+                {
+                    //Side effects like this in a predicate
+                    //are used here to demonstrate a principle
+                    //and should generally be avoided
+                    Console.WriteLine("\t" + patent);
+                }
+                return true;
+            });
+            Console.WriteLine("1. First patents prior to the 1900s:");
+            foreach (Patent patent in patents)
+            { 
+            }
+            Console.WriteLine($@"There are {patents.Count()} patents prior to 1900.");
+
+            Console.WriteLine();
+            Console.WriteLine("2. A second listing of patents prior to the 1900s:");
+            Console.WriteLine($@"There are {patents.Count()} patents prior to 1900.");
+
+            Console.WriteLine();
+            Console.WriteLine("3. A third listing of patents prior to the 1900s:");
+            patents = patents.ToArray();
+            Console.Write("  There are ");
+            Console.WriteLine($"{patents.Count()} patents prior to 1900.");
+        }
+    }
+}
