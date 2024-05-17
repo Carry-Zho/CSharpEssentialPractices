d039b2d3df6f9c248ae4be48f2cfaa3d974bea95
karry <karry@outlook.com>
2024-05-17 09:47:11 +0800

    Add 15.19

---
+++ Practice_15.18/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Practice_15._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Department> departments = CorporateData.Departments;
+            Print(departments);
+            Console.WriteLine();
+            IEnumerable<Employee> employees = CorporateData.Employees;
+            Print(employees);
+        }
+        private static void Print<T>(IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Practice_15.19/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace Practice_15._19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Department[] departments = CorporateData.Departments;
+            Employee[] employees = CorporateData.Employees;
+            IEnumerable<(int Id, string Name, string Title, Department Department)> items = 
+                employees.Join(departments,employee => employee.DepartmentId, department => department.Id, (employee, department) => (employee.Id, employee.Name, employee.Title, department));
+
+            foreach(var item in items) 
+            {
+                Console.WriteLine($"{item.Name} ({item.Title})");
+                Console.WriteLine("\t"+item.Department);
+            }
+        }
+    }
+}
