30c7c01ffdce05fc423302128524caf1a3b2bc55
karry <karry@outlook.com>
2024-05-30 10:36:26 +0800

    Add Practice_Namespace

---
+++ Practice_6.1/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_6._1
+{
+    internal class Employee
+    {
+    }
+}
---
+++ Practice_6.1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Practice_6._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2;
+            employee2 = new Employee();
+        }
+    }
+}
