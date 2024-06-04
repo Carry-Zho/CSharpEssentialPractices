02bc1b2d244025f3e47da6198a925a0442db98f9
karry <karry@outlook.com>
2024-06-04 15:11:46 +0800

    Add Practice_ManagedUnmanagedResources

---
+++ Practice_ManagedUnmanagedResources/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Practice_ManagedUnmanagedResources
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
