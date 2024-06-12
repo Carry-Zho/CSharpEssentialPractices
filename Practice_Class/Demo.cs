181229ad9103dd67300023bdf454a42123497a1f
karry <karry@outlook.com>
2024-06-12 23:23:28 +0800

    6/12

---
+++ Practice_Class/Demo.cs
@@ -0,0 +1,11 @@
+﻿namespace Practice_Class
+{
+    internal class Demo
+    {
+        public static int DemoOjbectCount;
+        public void SaySomething()
+        {
+            Console.WriteLine("Hello Moto");
+        }
+    }
+}
---
+++ Practice_Class/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Practice_Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MultiReturnFieldMethod().SayHi);
+            MultiReturnFieldMethod().dm.SaySomething();
+
+        }
+        public static (string SayHi, Demo dm) MultiReturnFieldMethod()
+        {
+            return ("Hi CSharp.", new Demo());
+        }
+    }
+}
