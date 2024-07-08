f41e616d5c492e9bd290848e57a506e20ef98f8d
karry <karry@outlook.com>
2024-07-08 12:29:10 +0800

    Close

---
+++ Practice_RefParameter/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_RefParameter
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public Person() { }
+        public Person(string name) { Name = name; }
+    }
+}
---
+++ Practice_RefParameter/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Practice_RefParameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Name = "张三";
+            Console.WriteLine(person.Name);
+            Print(in person);
+            Console.WriteLine(person.Name);
+        }
+                                                            static void Print(in Person para1)
+        {
+            Console.WriteLine(para1.Name);
+            para1.Name = "里斯";
+            para1 = new Person();
+        }
+    }
+}
