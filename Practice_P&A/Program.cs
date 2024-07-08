3a6b6978e60f8061d557b7e4268396c77835f203
karry <karry@outlook.com>
2024-07-08 23:09:43 +0800

    Close Practice_P&A

---
+++ Practice_P&A/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practice_P_A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Test(second:"C#!",first:"Hi");
+        }
+        static void Test(string first, string second)
+        {
+            Console.WriteLine(first + " " +second);
+        }
+    }
+}
+
+
+
+
