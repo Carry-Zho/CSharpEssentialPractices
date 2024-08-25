aba112b578e03cd91256071af5394532b20d7bc3
karry <karry@outlook.com>
2024-08-25 22:53:29 +0800

    Close

---
+++ Practice_Partial/PartialClass_Detail.cs
@@ -0,0 +1,9 @@
+﻿namespace Practice_Partial
+{
+    internal partial class PartialClass
+    {
+        public partial void Method1() { }
+        public partial void Method2() { }
+        public partial void Method3() { }
+    }
+}
